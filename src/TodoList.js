74320bf396ea2d46ae52fb71069d9ba058d60600
ZhongZhaoCong <535377683@qq.com>
2019-11-15 18:07:40 +0800

    feat(project): 项目基础页面

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css';
-import { Input } from 'antd';
+import { Input, Button, List } from 'antd';
+
+const data = [
+    '早8点开晨会，分配今天的开发工作',
+    '早9点和项目经理作开发需求讨论会',
+    '晚5:30对今日代码进行review'
+];
 
 class TodoList extends Component {
     render() { 
         return ( 
-            <div>
+            <div style={{margin:'10px'}}>
                 <div>
-                    <Input placeholder='请输入...' style={{ width:'250px' }} />
+                    <Input placeholder='请输入...' style={{ width:'250px', marginRight:'10px' }} />
+                    <Button type="primary">增加</Button>
+                </div>
+                <div style={{ margin:'10px',width:'300px' }}>
+                    <List
+                        bordered
+                        dataSource={ data }
+                        renderItem={ item => (<List.Item>{item}</List.Item>) }
+                    />    
                 </div>
             </div>
          );
